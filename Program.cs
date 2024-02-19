6a8d801681992867e04d247aeee15847829dc28b
nancycad01 <nancyacad01@gmail.com>
2024-02-19 16:02:17 +0530

    add files

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// using System;
+
+// namespace Program.cs
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       Console.WriteLine("Hi Nancy!");
+// int myInt = 5;               
+// double MyDouble = 5.99D;  
+// char myLetter = 'D';        
+// bool myBool = true;          
+// String mytext = "Hello";
+//  int sum1 = 100 + 50;     
+//       int sum2 = sum1 + 250;   
+//       int sum3 = sum2 + sum2; 
+//       int x = 10;
+//       Console.WriteLine(x);
+// Console.WriteLine(myInt);
+// Console.WriteLine(MyDouble);
+// Console.WriteLine(myLetter);
+// Console.WriteLine(myBool);
+// Console.WriteLine(mytext);
+//       Console.WriteLine(sum1);
+//       Console.WriteLine(sum2);
+//       Console.WriteLine(sum3);       
+//       int N = 5;
+//     int J = 10;
+//     Console.WriteLine(N);
+//     Console.WriteLine(J);
+
+//     }
+//   }
+// }
+/*using System;
+
+class Program
+{
+  static void MyMethod(string fname ,int age)
+  {
+    Console.WriteLine(fname + "is" + age);
+  }
+  static void Main(string[] args)
+  { 
+  MyMethod("John",28);
+  MyMethod("Pavithra",22);
+  MyMethod("Dinesh",24);
+  }
+}
+*/
+using System;
+namespace MyApplication{
+  class Program{
+  static void Main(string[] args)
+  {
+    Console.WriteLine(Math.Max(5,10));
+      Console.WriteLine(Math.Sqrt(64));  
+    Console.WriteLine(Math.Min(5,10));
+    Console.WriteLine(Math.Abs(-2.5));
+    Console.WriteLine(Math.Round(10.99));
+  }
+}
+}
